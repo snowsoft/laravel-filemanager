a9b2485d0c129ea89270015ecc58311df8c800fe
snowsoft <abdulkadirkar@gmail.com>
2022-04-19 14:30:52 +0300

    --
    -

--- src/Handlers/ConfigHandler.php
+++ src/Handlers/ConfigHandler.php
@@ -6,6 +6,6 @@ class ConfigHandler
 {
     public function userField()
     {
-        return \Plugins\EnvJson::setdata()->get('APP_USERNAME', 'demo');
+        return Config('app.mediafolder');
     }
 }
